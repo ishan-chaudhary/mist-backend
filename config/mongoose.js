20733469d74d1a469248a3ac562da6ab2aa5332c
ishan chaudhary <ishan4220@gmail.com>
2021-08-02 11:56:01 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
+
+const db=mongoose.connection;
+
+db.on('error',console.error.bind(console,'error in connecting to db'));
+
+db.once('open',function(){
+console.log('successfully connected to database');
+})
+
+module.exports=db;
